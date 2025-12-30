ccb70a44c495244db1d1d88514be1b18060a229e
AustinBray77 <austin.bray77@gmail.com>
2025-12-30 16:47:25 -0500

    Initial build

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use std::{fs::File, io::Read};
+
+use arboard;
+use rpassword;
+use rsd_encrypt::legacy_decrypt;
+mod io;
+mod types;
+
+use io::read_u32;
+use types::AnyResult;
+
+use crate::types::{Account, JsonAccount};
+
+fn decrypt_contents(contents: String) -> AnyResult<String> {
+    println!("Enter password:");
+
+    let encryption_key = rpassword::read_password()?;
+
+    // TODO: Modify the rsd_encrypt crate so the Error impl std::error::Error
+    // Will allow me to get rid of this expect
+    Ok(legacy_decrypt(contents, encryption_key).expect("Unable to decrypt the file"))
+}
+
+fn print_account((ind, account): (usize, &Account)) {
+    println!("{}) {}", ind, account);
+}
+
+fn parse_contents(contents: String) -> AnyResult<Vec<Account>> {
+    let json_accounts: Vec<JsonAccount> = serde_json::from_str(&contents)?;
+
+    Ok(json_accounts
+        .into_iter()
+        .map(Into::<Account>::into)
+        .collect())
+}
+
+// TODO: Possibly move to better error handling
+// I feel like panicing in main is probably fine tho
+fn main() {
+    let src_path: &str = "/home/aus/Documents/psd.bin";
+
+    let mut src_file = File::open(src_path).expect("Expected file to exist, panicing...");
+
+    let mut enc_contents = String::new();
+
+    src_file
+        .read_to_string(&mut enc_contents)
+        .expect("Unable to read file contents, panicing...");
+
+    let contents = decrypt_contents(enc_contents).expect("Unable to decrypt the file");
+
+    let accounts: Box<[Account]> = parse_contents(contents)
+        .expect("Unable to parse decrypted file")
+        .into();
+
+    println!("Available PSDs");
+
+    accounts.iter().enumerate().for_each(print_account);
+
+    println!("Which account would you like to copy to clipboard?");
+
+    let inputted_num = read_u32(accounts.len() as u32).expect("Invalid input, panicing...");
+
+    let account = &accounts[inputted_num as usize];
+
+    let clipboard = arboard::Clipboard::new().expect("Unable to initialize clipboard");
+
+    account.copy_pass(clipboard);
+
+    println!("Copied password for {} to clipboard.", account);
+}
---
+++ src/types.rs
@@ -0,0 +1,38 @@
+use std::{error::Error, fmt::Display};
+
+use arboard::Clipboard;
+
+#[allow(non_snake_case)] // Allowed due to the way the old files are serialized
+#[derive(serde::Deserialize)]
+pub struct JsonAccount {
+    Name: String,
+    Password: String,
+}
+
+pub struct Account {
+    name: String,
+    password: String,
+}
+
+impl Account {
+    pub fn copy_pass(&self, mut clip: Clipboard) {
+        clip.set_text(self.password.clone())
+            .expect("Unable to copy value to clipboard");
+    }
+}
+
+impl From<JsonAccount> for Account {
+    fn from(value: JsonAccount) -> Self {
+        Account {
+            name: value.Name,
+            password: value.Password,
+        }
+    }
+}
+
+impl Display for Account {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+pub type AnyResult<T> = Result<T, Box<dyn Error>>;
